4732520378369737c58ad9fd6abc685562686385
jacobsanchezbejarano <jake_76847@hotmail.com>
2022-09-13 14:06:46 -0400

    adding file of game

---
+++ tictactoe.py
@@ -0,0 +1,24 @@
+'''
+
+CSE210 
+W01 Prove: Developer
+Author: Jacob Sanchez Bejarano
+
+'''
+
+def main():
+    pass
+
+
+def print_template():
+    print('''
+    1|2|3
+    -+-+-
+    4|5|6
+    -+-+-
+    7|8|9 
+    ''')
+
+
+if __name__ == '__main__':
+    main();
